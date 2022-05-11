55a8edef74931e334bab5995b9a10d5628c69941
Amadi Chimeremma <chimesandra@yahoo.com>
2022-05-11 17:16:06 +0100

    commit git

---
+++ data.py
@@ -0,0 +1,53 @@
+quiz_data = [
+    {
+        "question": " You are a new member of the Nupat studentship family? (True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": "you already knew a thing or two about tech before joining Nupat? (True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": " Have you purchased any of our products before? (True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": "Do you feel rewarded for your loyalty to Nupat (True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": " Do you recognise the company Logo(True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": " Has your life been improved ever since you joined Nupat? (True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": "Has Nupat failed to meet your expectations in time? (True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": "Would you like to work for Nupat? (True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": "Do you trust Nupat enough to buy the new product we are about to launch? (True/false)",
+        "correct_answer": "True"
+    },
+
+    {
+        "question": "Do you feel social media coverage of Nupat has been mostly positive or mostly negative in the past year (True/false)",
+        "correct_answer": "True"
+    },
+
+    
+]
